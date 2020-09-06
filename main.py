1fb8b9fd1cc0bda07b624360a855b103b6bd051a
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-09-06 16:29:03 +0200

    ok

---
+++ main.py
@@ -0,0 +1,29 @@
+from Packets import BeaconPacket
+from Packets import Packets
+import socket
+from configparser import ConfigParser
+config = ConfigParser()
+config.read('config.ini')
+
+
+p1 = BeaconPacket("1","10.10.0.1","10.10.0.0","100","10.10.0.1")
+print(p1.printFullPacket())
+print(p1.printLitePacket())
+
+p1.getBytesFromPackets() 
+
+test = bytearray(b'-1--46------10.10.0.1------10.10.0.0-0100------10.10.0.1Payload BEACON')
+p2 = Packets.getPacketFromBytes(test)
+
+print(p2.printFullPacket())
+print(p2.printLitePacket())
+
+
+
+print(socket)
+
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+
+print(f"Hostname: {hostname}")
+print(f"IP Address: {ip_address}")
