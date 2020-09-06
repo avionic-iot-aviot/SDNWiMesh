d69f4de962c280b4afc03229254537a40d03f94a
Carmelo Ricci <carmeloricci@MBP-di-Carmelo.lan>
2020-09-06 20:27:54 +0200

    ok

--- init_config.py
+++ init_config.py
@@ -9,7 +9,7 @@ config.read('config.ini')
 def Inizio():
     if ( socket.gethostname() == "Omega-1D63"):
         # subprocess.call(["uci set network.wlan.ipaddr=192.168.0.1" , "uci commit network", "/etc/init.d/network restart"])
-        subprocess.call(["pwd"])
+        subprocess.call(["pwd", "pwd", "ls"])
         sys.stdout.flush()
         print("Tutto ok")
     if ( socket.gethostname() == "Omega-1D06"):
