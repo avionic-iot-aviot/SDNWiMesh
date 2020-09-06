ac05d0e28831ca9be5c9bdc25c9a882f86736eaa
Carmelo Ricci <carmeloricci@MBP-di-Carmelo.lan>
2020-09-06 20:35:32 +0200

    ok

--- init_config.py
+++ init_config.py
@@ -2,6 +2,7 @@ import subprocess
 import socket
 import sys
 import os
+import time
 from configparser import ConfigParser
 config = ConfigParser()
 config.read('config.ini')
@@ -9,11 +10,12 @@ config.read('config.ini')
 
 def Inizio():
     if ( socket.gethostname() == "Omega-1D63"):
-        os.system('uci set network.wlan.ipaddr=192.168.0.1')
+        temp = config['Omega-1D63']['IpStation']
+        os.system(f'uci set network.wlan.ipaddr={temp}')
         os.system('uci commit network')
         os.system('/etc/init.d/network restart')
-        # subprocess.call(["uci set network.wlan.ipaddr=192.168.0.1" , "uci commit network", "/etc/init.d/network restart"])
         sys.stdout.flush()
+        time.sleep(5)
         print("Tutto ok")
     if ( socket.gethostname() == "Omega-1D06"):
         subprocess.call(["uci set network.wlan.ipaddr=192.168.1.1" , "uci commit network", "/etc/init.d/network restart"])
