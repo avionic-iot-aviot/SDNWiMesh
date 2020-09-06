4c7d87b1ad473161bc6a80105c335ff31de1256b
Carmelo Ricci <carmeloricci@MBP-di-Carmelo.lan>
2020-09-06 21:03:00 +0200

    ok

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from Packets import Packets
 import init_config
 import socket
 import ifaddr
+import time
 from configparser import ConfigParser
 config = ConfigParser()
 config.read('config.ini')
@@ -40,3 +41,25 @@ for adapter in adapters:
         IpClient = adapter.ips[0].ip
 
 print(f"Client: {IpClient} \tStation: {IpStation}")
+
+if ( socket.gethostname() == "Omega-1D63"):
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    server.settimeout(0.2)
+    message = b"your very important message"
+    while True:
+        server.sendto(message, ('<broadcast>', 37020))
+        print("message sent!")
+        time.sleep(1)
+
+if ( socket.gethostname() == "Omega-1D06"):
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) # UDP
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    client.bind(("", 37020))
+    while True:
+        data, addr = client.recvfrom(1024)
+        print("received message: %s"%data)
