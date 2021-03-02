a0793b96cf75634c6a4c65291b9f73875b54260e
Salvatore Quattropani <salvo41993@gmail.com>
2021-03-02 22:59:41 +0100

    test4362872

--- UDP_Socket.py
+++ UDP_Socket.py
@@ -154,16 +154,15 @@ def PrintBasicInfo(NeighborInfo,OtherInfo):
 
 
 
-#class ThreadScanNet (threading.Thread):
- #  def __init__(self, threadID, name):
-   #   threading.Thread.__init__(self)
-   #   self.threadID = threadID
-   #   self.name = name
+class ThreadScanNet (threading.Thread):
+   def __init__(self, threadID, name):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
 
-  # def run(self):
-    #  while True:
-         
-     #    print("Starting " + self.name)
-      #   init_config.ScanNetwork()
-     #    time.sleep(int(config['GENERAL']['ScanNetSleep']))
+   def run(self):
+      while True:
+         print("Starting " + self.name)
+         init_config.ScanNetwork()
+         time.sleep(int(config['GENERAL']['ScanNetSleep']))
          
