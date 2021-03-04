9acba83dac4a7d06cb92864d29fa721df2c1b399
Salvatore Quattropani <salvo41993@gmail.com>
2021-03-04 13:37:19 +0100

    asdfd

--- provo.py
+++ provo.py
@@ -1,11 +1,19 @@
-
-
+import socket
 import sys
-import os
 
 
-import subprocess
+ip = "192.168.1.152"
+port = 4100
 
-result = subprocess.run(['arp -a | awk \'{print $1,$4}\''], stdout=subprocess.PIPE)
-print(result.stdout)
 
+# Create a UDP socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Bind the socket to the port
+server_address = (ip, port)
+s.bind(server_address)
+
+
+while True:
+    print("####### Server is listening #######")
+    data, address = s.recvfrom(4096)
+    print("\n\n 2. Server received: ", data.decode('utf-8'), "\n\n")
