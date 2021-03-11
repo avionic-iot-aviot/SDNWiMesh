65e11e92bc6f95e901bc2d4103cda0b94130acdd
Salvatore Quattropani <salvo41993@gmail.com>
2021-03-11 13:43:45 +0100

    ds

--- AudioFile.py
+++ AudioFile.py
@@ -84,7 +84,7 @@ class ThreadAud (threading.Thread):
             payload = ""
             while len(payload) <= 5000:
                 payload = payload+" " +str(ser.readline().decode("utf-8")).rstrip()
-                print(len(payload))
+                #print(len(payload))
 
             print("send mic data:", payload)
             pckData = DataPacket(config['GENERAL']['NetId'], config['GENERAL']['IpSink'], init_config.GetIp(
