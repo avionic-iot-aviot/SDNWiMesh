cba3423764882205ad5d5d5af332f1a94e43dcab
Salvatore Quattropani <salvo41993@gmail.com>
2021-05-04 14:30:05 +0200

    fds

--- AudioFile.py
+++ AudioFile.py
@@ -92,7 +92,7 @@ class ThreadAud (threading.Thread):
         while node_variables.MicStatus == "ON":
             print("Microphone " + self.action)
             audioSample = []
-            while len(audioSample) <= 10:
+            while len(audioSample) <= 1000:
                 messRCV = False
                 strVal = ser.read(1)
                 print(strVal)
