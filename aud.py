3b224fa480a0d183557418c3a16862c23bb0464a
Marco Cavalli <cmarcos46@gmail.com>
2021-07-20 11:52:37 +0200

    Updated with ruben's new file. Now: - It records samples in 2 bytes instead of 3 (we still have a byte used as delimitatore so 1+2 for each sample) - It now uses a buffer to store samples before sending them to the sink

--- aud.py
+++ aud.py
@@ -1,44 +1,72 @@
 import serial
-# Init buffer containing the received data
-buffSize = 3
-inBuff = [bytes(0)] * buffSize
-pos = 0
+
+onionCode = 0
+
+#Init buffer containing the received data
+buffSize = 360
+
 
 done = False
-messRCV = False
 sampleSizeRCV = False
 audioSample = []
+counter = 0
 
-
-# Create serial object
-ser = serial.Serial('/dev/ttyUSB1', baudrate=12000000,rtscts=True,dsrdtr=True, timeout=0.1)
-# Open serial
-
+if onionCode == 1:
+    port = '/dev/ttyACM0'
+else:
+    port = 'COM6'
+    
+baudrate = 4000000
 try:
+    #Create serial object
+    ser = serial.Serial(None,baudrate, timeout = 0.1)
+    #Open serial
+    ser.port = port
     ser.open()
-except:
-    print("gia aperta")
-
-# Start looking for the message
-while done == False:
-    messRCV = False
-    strVal = ser.read(1)
-
-    if strVal == ('<').encode():
-        while not messRCV:
-            # If message found or buff is full stop reading and save sample
-
-            if pos == buffSize:  # and val == ('\r').encode():
-
-                audioSample.append(int.from_bytes(inBuff[0] + inBuff[1] + inBuff[2], "big", signed="True"))
-                print(len(audioSample))
-                messRCV = True
-                pos = 0
-                inBuff = [bytes(0)] * buffSize
-
-                # ser.reset_output_buffer()
+    #Start data transmission 
+    ser.write('start'.encode('utf-8'))
+    #Start looking for the message
+    while len(audioSample) < 60000 :
+        counter = 0
+        inBuff = ser.read(buffSize)
+        #print("Waiting packets: ",ser.in_waiting)
+        while counter < buffSize:
+            if inBuff[counter:counter+1] ==  ('0').encode():
+                audioSample.append(int.from_bytes(inBuff[counter+1:counter+2]+inBuff[counter+2:counter+3],"big", signed="True"))
+                counter = counter + 3
             else:
-                val = ser.read(1)
-                inBuff[pos] = bytes(val)
-                pos = pos + 1
+                counter = counter + 1
+                if counter + 2 > buffSize:
+                    inBuff = bytearray(buffSize)
+                    break
+                        
+except :
+    print('Program exit !')
+    
+finally :
+    ser.write('stop'.encode('utf-8'))
+    maxTest = 0
+    
+    ser.reset_input_buffer()
+    while sampleSizeRCV != True and maxTest < 10:
+        mss = str(ser.read_until(expected= '\n'))
+        index = mss.rfind("Sample sent:")
+        if index == -1:
+            maxTest = maxTest + 1
+        else:
+            sampleRCV = mss[index:len(mss)-5]
+            sampleSizeRCV = True
 
+        
+    ser.close()
+    #Save normalized file to text
+    if onionCode == 1:
+        fHandler = open('/home/microphone/audio.txt', 'w')
+    else:
+        fHandler = open('C:/Users/ruben/Desktop/Test_9.txt', 'w')
+        
+    for line in audioSample:
+        fHandler.write(str(line))
+        fHandler.write("\n")
+    fHandler.close()
+pass
