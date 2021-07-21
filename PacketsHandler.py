553be1a996074b40329e0286348162ba4649aab1
Marco Cavalli <cmarcos46@gmail.com>
2021-07-21 14:45:03 +0200

    Fixed PacketsHandler.py printing logs even when it shouldn't do it

--- PacketsHandler.py
+++ PacketsHandler.py
@@ -9,6 +9,7 @@ import UDP_Socket
 import threading
 import node_variables
 from configparser import ConfigParser
+
 config = ConfigParser()
 config.read('/etc/SDNPy-SDNWiMesh/config.ini')
 from AudioFile import GetAudio
@@ -19,89 +20,109 @@ from Packets import DataPacket
 
 def PacketHandler(data, address):
     packet = Packets.getPacketFromBytes(data)
-    print (packet.printLitePacket())
-    print("--->", init_config.GetIp(config['GENERAL']['StationInterface']))
+    if config['DEBUG']['PRINT_LOGS'] is True:
+        print(packet.printLitePacket())
+    if config['DEBUG']['PRINT_LOGS'] is True:
+        print("--->", init_config.GetIp(config['GENERAL']['StationInterface']))
 
-    if (packet.Destination == init_config.GetIp(config['GENERAL']['StationInterface'])):
+    if (packet.Destination == init_config.GetIp(
+            config['GENERAL']['StationInterface'])):
         #print ("Yess - è per me")
         # print("######## ", packet.TTL)
-        if(int(packet.Type) == 0):
+        if (int(packet.Type) == 0):
             TypeBeacon(packet)
-        if(int(packet.Type) == 1):
+        if (int(packet.Type) == 1):
             TypeReport(packet)
-        if(int(packet.Type) == 2):
+        if (int(packet.Type) == 2):
             TypeData(packet, packet.Source)
-        if(int(packet.Type) == 3):
+        if (int(packet.Type) == 3):
             TypeFunction(packet)
     #else:
-        #print("NO - non è per me")
-       # packet.DecreaseTTL()
-        #print("@@@@@@@ ", packet.TTL)
-       # data = packet.getBytesFromPackets()
-       # UDP_Socket.SendUdpPacketUnicast(
-            #data, node_variables.IpDefaultGateway, int(config['GENERAL']['Port']))
-    if (packet.NextHop == str(config['GENERAL']['IpSinkOnWan'])and init_config.GetIp(config['GENERAL']['StationInterface'])==str(config['GENERAL']['IpSink'])):        
-        if(int(packet.Type) == 3):
+    #print("NO - non è per me")
+    # packet.DecreaseTTL()
+    #print("@@@@@@@ ", packet.TTL)
+    # data = packet.getBytesFromPackets()
+    # UDP_Socket.SendUdpPacketUnicast(
+    #data, node_variables.IpDefaultGateway, int(config['GENERAL']['Port']))
+    if (packet.NextHop == str(config['GENERAL']['IpSinkOnWan'])
+            and init_config.GetIp(config['GENERAL']['StationInterface'])
+            == str(config['GENERAL']['IpSink'])):
+        if (int(packet.Type) == 3):
             TypeFunction(packet)
-    
 
 
 def TypeBeacon(packet):
 
-    
-    if (packet.Destination==str(config['GENERAL']['IpSink']) and init_config.GetIp(config['GENERAL']['StationInterface'])==str(config['GENERAL']['IpSink']) ):
+    if (packet.Destination == str(config['GENERAL']['IpSink'])
+            and init_config.GetIp(config['GENERAL']['StationInterface'])
+            == str(config['GENERAL']['IpSink'])):
 
         #Packets.getPacketFromBytes(data).printLitePacket()
-        print("Beacon Received from: ", packet.Source)
+        if config['DEBUG']['PRINT_LOGS'] is True:
+            print("Beacon Received from: ", packet.Source)
         data = packet.getBytesFromPackets()
-        UDP_Socket.SendUdpPacketUnicast(data, config['GENERAL']['IpController'], int(config['GENERAL']['PortController']))
-    
+        UDP_Socket.SendUdpPacketUnicast(
+            data, config['GENERAL']['IpController'],
+            int(config['GENERAL']['PortController']))
 
 
 def TypeReport(packet):
-    if (packet.Destination==str(config['GENERAL']['IpSink'])and init_config.GetIp(config['GENERAL']['StationInterface'])==str(config['GENERAL']['IpSink'])):
-        print("Report Received from: ", packet.Source)
+    if (packet.Destination == str(config['GENERAL']['IpSink'])
+            and init_config.GetIp(config['GENERAL']['StationInterface'])
+            == str(config['GENERAL']['IpSink'])):
+        if config['DEBUG']['PRINT_LOGS'] is True:
+            print("Report Received from: ", packet.Source)
         data = packet.getBytesFromPackets()
-        UDP_Socket.SendUdpPacketUnicast(data, config['GENERAL']['IpController'], int(config['GENERAL']['PortController']))
+        UDP_Socket.SendUdpPacketUnicast(
+            data, config['GENERAL']['IpController'],
+            int(config['GENERAL']['PortController']))
 
 
-def TypeData(packet,s):
+def TypeData(packet, s):
     #if (packet.Destination==str(config['GENERAL']['IpSink'])and init_config.GetIp(config['GENERAL']['StationInterface'])==str(config['GENERAL']['IpSink'])): no
-    print("Data Received from: ", s)
-    pckData = DataPacket(
-        config['GENERAL']['NetId'],
-        config['GENERAL']['IpRasp'],
-        config['GENERAL']['IpSink'],
-        "100",
-        config['GENERAL']['IpRasp'],
-        packet.Payload
-    )
+    if config['DEBUG']['PRINT_LOGS'] is True:
+        print("Data Received from: ", s)
+    pckData = DataPacket(config['GENERAL']['NetId'],
+                         config['GENERAL']['IpRasp'],
+                         config['GENERAL']['IpSink'], "100",
+                         config['GENERAL']['IpRasp'], packet.Payload)
     data = pckData.getBytesFromPackets()
-    UDP_Socket.SendUdpPacketUnicast(data, config['GENERAL']['IPRasp'], int(config['GENERAL']['PortRasp']))
+    UDP_Socket.SendUdpPacketUnicast(data, config['GENERAL']['IPRasp'],
+                                    int(config['GENERAL']['PortRasp']))
     #print("Salvo su file------->", str(packet.Payload) )
     #f = open("/etc/AUDIO/sample.txt", "wb") #commen
     #f. write(packet.Payload) #comme
     #f. close() #comm
 
 
-
 def TypeFunction(packet):
-    print("Sono nella TypeFunction")
-    print("Next hop->", packet.NextHop ," vs->", str(config['GENERAL']['IpSinkOnWan']))
+    if config['DEBUG']['PRINT_LOGS'] is True:
+        print("Sono nella TypeFunction")
+    if config['DEBUG']['PRINT_LOGS'] is True:
+        print("Next hop->", packet.NextHop, " vs->",
+              str(config['GENERAL']['IpSinkOnWan']))
     #config['GENERAL']['StationWanInterface']
-    print("Wan IP->", init_config.GetIp("br-lan1") ," vs->",str(config['GENERAL']['IpSinkOnWan']))
-    print("packet destnation->",packet.Destination , " vs->",init_config.GetIp(config['GENERAL']['StationInterface'])) 
-    if (packet.NextHop==str(config['GENERAL']['IpSinkOnWan']) and init_config.GetIp("br-lan1")==str(config['GENERAL']['IpSinkOnWan'])):
-        print("Function PKT Received from: ", packet.Source)
+    if config['DEBUG']['PRINT_LOGS'] is True:
+        print("Wan IP->", init_config.GetIp("br-lan1"), " vs->",
+              str(config['GENERAL']['IpSinkOnWan']))
+    if config['DEBUG']['PRINT_LOGS'] is True:
+        print("packet destnation->", packet.Destination, " vs->",
+              init_config.GetIp(config['GENERAL']['StationInterface']))
+    if (packet.NextHop == str(config['GENERAL']['IpSinkOnWan'])
+            and init_config.GetIp("br-lan1") == str(
+                config['GENERAL']['IpSinkOnWan'])):
+        if config['DEBUG']['PRINT_LOGS'] is True:
+            print("Function PKT Received from: ", packet.Source)
         dest = packet.Destination
         data = packet.getBytesFromPackets()
-        print("Invio a, " ,dest)
-        UDP_Socket.SendUdpPacketUnicast(data,dest, int(config['GENERAL']['Port']))
-    if (packet.Destination == init_config.GetIp(config['GENERAL']['StationInterface'])):
+        if config['DEBUG']['PRINT_LOGS'] is True:
+            print("Invio a, ", dest)
+        UDP_Socket.SendUdpPacketUnicast(data, dest,
+                                        int(config['GENERAL']['Port']))
+    if (packet.Destination == init_config.GetIp(
+            config['GENERAL']['StationInterface'])):
         GetAudio(packet.Payload)
         #ciao
-
-
 
 
 def UpdateNeighborList(ip):
