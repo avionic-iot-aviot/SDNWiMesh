ad1c0fb5a4e0c21ee073a80a4e311ec526c7afc8
Marco Cavalli <cmarcos46@gmail.com>
2021-07-21 14:43:55 +0200

    - Added a variable in config.ini used to turn on and off logs - All debug prints will be filtered if the variable in config.ini called "SHOW_LOGS" is False

--- AudioFile.py
+++ AudioFile.py
@@ -16,12 +16,9 @@ import node_variables
 import init_config
 from configparser import ConfigParser
 import serial
+
 config = ConfigParser()
 config.read('/etc/SDNPy-SDNWiMesh/config.ini')
-
-
-
-
 
 # class ThreadSendDataAudio (threading.Thread):
 #   def __init__(self, threadID, name):
@@ -49,29 +46,29 @@ def GetAudio(action):
         # return
 
     if action.decode('utf-8') == "OFF":
-        print("Microphone "+action.decode('utf-8'))
+        print("Microphone " + action.decode('utf-8'))
         node_variables.MicStatus = action
         #os.system("kill -SIGKILL " + str(node_variables.ThreadId))
 
         #obj = wave.open('/tmp/SDNPy-master/sound.wav', 'r')
-       # print("Number of channels", obj.getnchannels())
-        #print("Sample width", obj.getsampwidth())
-        #print("Frame rate.", obj.getframerate())
-        #print("Number of frames", obj.getnframes())
-        #print("parameters:", obj.getparams())
-        #frame = 0
-        # for x in range(0, 2601600, 80):
-        #tmp = obj.readframes(int(config['FileWave']['Frame']))
-        #pckData = DataPacket(config['GENERAL']['NetId'],config['GENERAL']['IpSink'], nodeIP, "100",config['GENERAL']['IpSink'],'CIAOOONE')
-        # UDP_Socket.SendUdpPacketUnicast(pckData.getBytesFromPackets(),config['GENERAL']['IpSink'],int(config['GENERAL']['Port']))
-        #frame = frame + len(tmp)
-        #print("Frame ["+ str(x) + "]   lette: " + str(frame))
-        # time.sleep(float(config['FileWave']['TimeSleepBetweenTwoFrame']))
-        # obj.close()
-        # time.sleep(int(config['FileWave']['TimeSleepBetweenTwoPlay']))
+    # print("Number of channels", obj.getnchannels())
+    #print("Sample width", obj.getsampwidth())
+    #print("Frame rate.", obj.getframerate())
+    #print("Number of frames", obj.getnframes())
+    #print("parameters:", obj.getparams())
+    #frame = 0
+    # for x in range(0, 2601600, 80):
+    #tmp = obj.readframes(int(config['FileWave']['Frame']))
+    #pckData = DataPacket(config['GENERAL']['NetId'],config['GENERAL']['IpSink'], nodeIP, "100",config['GENERAL']['IpSink'],'CIAOOONE')
+    # UDP_Socket.SendUdpPacketUnicast(pckData.getBytesFromPackets(),config['GENERAL']['IpSink'],int(config['GENERAL']['Port']))
+    #frame = frame + len(tmp)
+    #print("Frame ["+ str(x) + "]   lette: " + str(frame))
+    # time.sleep(float(config['FileWave']['TimeSleepBetweenTwoFrame']))
+    # obj.close()
+    # time.sleep(int(config['FileWave']['TimeSleepBetweenTwoPlay']))
 
 
-class ThreadAud (threading.Thread):
+class ThreadAud(threading.Thread):
     def __init__(self, name, action):
         threading.Thread.__init__(self)
         self.name = name
@@ -79,20 +76,27 @@ class ThreadAud (threading.Thread):
 
     def run(self):
         node_variables.MicStatus = self.action
-        print("Avvio il Thread all'interno")
-        ser = serial.Serial('/dev/ttyUSB1', baudrate=12000000,rtscts=True,dsrdtr=True, timeout=0)
-        print(ser.isOpen())
-        pos=0
+        if config['DEBUG']['PRINT_LOGS'] is True:
+            print("Avvio il Thread all'interno")
+        ser = serial.Serial('/dev/ttyUSB1',
+                            baudrate=12000000,
+                            rtscts=True,
+                            dsrdtr=True,
+                            timeout=0)
+        if config['DEBUG']['PRINT_LOGS'] is True:
+            print(ser.isOpen())
+        pos = 0
         buffSize = 3
         inBuff = [bytes(0)] * buffSize
         messRCV = False
         sampleSizeRCV = False
         audioSample = []
-        print("Eccomi")
+        if config['DEBUG']['PRINT_LOGS'] is True:
+            print("Eccomi")
         while node_variables.MicStatus == "ON":
             print("Microphone " + self.action)
             #audioSample = []
-            audioSample= bytearray()
+            audioSample = bytearray()
             while len(audioSample) <= 1000:
                 messRCV = False
                 strVal = ser.read(1)
@@ -100,11 +104,14 @@ class ThreadAud (threading.Thread):
                 if strVal == ('<').encode():
                     while not messRCV:
                         if pos == buffSize:  # and val == ('\r').encode():
-                            
+
                             #print("---------->", int.from_bytes(inBuff[0] + inBuff[1] + inBuff[2], "big", signed="True") )
-                            print("---------->",inBuff[0] + inBuff[1] + inBuff[2])
-                            audioSample.extend(inBuff[0] + inBuff[1] + inBuff[2])
-                            #audioSample.append(int.from_bytes(inBuff[0] + inBuff[1] + inBuff[2], "big", signed="True"))                            
+                            if config['DEBUG']['PRINT_LOGS'] is True:
+                                print("---------->",
+                                      inBuff[0] + inBuff[1] + inBuff[2])
+                            audioSample.extend(inBuff[0] + inBuff[1] +
+                                               inBuff[2])
+                            #audioSample.append(int.from_bytes(inBuff[0] + inBuff[1] + inBuff[2], "big", signed="True"))
                             messRCV = True
                             pos = 0
                             inBuff = [bytes(0)] * buffSize
@@ -112,16 +119,19 @@ class ThreadAud (threading.Thread):
                             val = ser.read(1)
                             inBuff[pos] = bytes(val)
                             pos = pos + 1
-              
 
-            print("send mic data:", str(audioSample))
-            pckData = DataPacket(config['GENERAL']['NetId'], config['GENERAL']['IpSink'], init_config.GetIp(
-                config['GENERAL']['StationInterface']), "100", config['GENERAL']['IpSink'], audioSample)
+            if config['DEBUG']['PRINT_LOGS'] is True:
+                print("send mic data:", str(audioSample))
+            pckData = DataPacket(
+                config['GENERAL']['NetId'], config['GENERAL']['IpSink'],
+                init_config.GetIp(config['GENERAL']['StationInterface']),
+                "100", config['GENERAL']['IpSink'], audioSample)
             # pckData="-1--38----192.168.3.1----192.168.3.1-2100----192.168.3.1"+payload
             # pckData=pckData.replace("192.168.3.1-2100",init_config.GetIp(config['GENERAL']['StationInterface'])+"-2100")
             #pckData=pckData.replace("38", str(38+len(payload)))
 
-            UDP_Socket.SendUdpPacketUnicast(pckData.getBytesFromPackets(), config['GENERAL']['IpSink'], int(config['GENERAL']['Port']))
-        
+            UDP_Socket.SendUdpPacketUnicast(pckData.getBytesFromPackets(),
+                                            config['GENERAL']['IpSink'],
+                                            int(config['GENERAL']['Port']))
 
             # UDP_Socket.SendUdpPacketUnicast(pckData.getBytesFromPackets(),config['GENERAL']['IpSink'],int(config['GENERAL']['Port']))
--- Packets.py
+++ Packets.py
@@ -7,13 +7,14 @@ import time
 import threading
 import node_variables
 from configparser import ConfigParser
+
 config = ConfigParser()
 config.read('/etc/SDNPy-SDNWiMesh/config.ini')
 
 
 class Packets:
-
-    def __init__(self,NetId,Length,Destination,Source,Type,TTL,NextHop,Payload):
+    def __init__(self, NetId, Length, Destination, Source, Type, TTL, NextHop,
+                 Payload):
         self.NetId = NetId
         self.Length = Length
         self.Destination = Destination
@@ -26,12 +27,12 @@ class Packets:
 
     def printFullPacket(self):
         return f"\nNetId:[{self.NetId}] Length:[{self.Length}] Destination:[{self.Destination}] Source:[{self.Source}] Type:[{self.Type}] TTL:[{self.TTL}] NextHop:[{self.NextHop}] Payload:[{self.Payload}]\n"
-    
+
     def printLitePacket(self):
         return f"\n[{self.NetId}]  [{self.Length}]  [{self.Destination}]  [{self.Source}]  [{self.Type}]  [{self.TTL}]  [{self.NextHop}]  [{self.Payload}]\n"
 
     @classmethod
-    def getPacketFromBytes(cls,frame):
+    def getPacketFromBytes(cls, frame):
 
         range1 = int(config['PACKET']['LenNetId'])
         range2 = range1 + int(config['PACKET']['LenLength'])
@@ -55,7 +56,7 @@ class Packets:
 
         while frame2.decode("utf-8")[0] == "-":
             frame2 = frame2[1:]
-        
+
         while frame3.decode("utf-8")[0] == "-":
             frame3 = frame3[1:]
 
@@ -71,83 +72,95 @@ class Packets:
         while frame7.decode("utf-8")[0] == "-":
             frame7 = frame7[1:]
 
-        if isinstance (frame8, str):
-            return cls(frame1.decode("utf-8"), frame2.decode("utf-8"), frame3.decode("utf-8"), frame4.decode("utf-8"), frame5.decode("utf-8"), frame6.decode("utf-8"), frame7.decode("utf-8"), frame8.decode("utf-8") )
+        if isinstance(frame8, str):
+            return cls(frame1.decode("utf-8"), frame2.decode("utf-8"),
+                       frame3.decode("utf-8"), frame4.decode("utf-8"),
+                       frame5.decode("utf-8"), frame6.decode("utf-8"),
+                       frame7.decode("utf-8"), frame8.decode("utf-8"))
         else:
-            return cls(frame1.decode("utf-8"), frame2.decode("utf-8"), frame3.decode("utf-8"), frame4.decode("utf-8"), frame5.decode("utf-8"), frame6.decode("utf-8"), frame7.decode("utf-8"), frame8)
-       
-        
+            return cls(frame1.decode("utf-8"), frame2.decode("utf-8"),
+                       frame3.decode("utf-8"), frame4.decode("utf-8"),
+                       frame5.decode("utf-8"), frame6.decode("utf-8"),
+                       frame7.decode("utf-8"), frame8)
+
     def getBytesFromPackets(self):
         self.fixTheLen()
-        
-        if ( (len(self.NetId) == int(config['PACKET']['LenNetId']))  and (len(self.Length) == int(config['PACKET']['LenLength'])) and (len(self.Destination) == int(config['PACKET']['LenDestination'])) and (len(self.Source) == int(config['PACKET']['LenSource'])) and (len(self.Type) == int(config['PACKET']['LenType'])) and (len(self.TTL) == int(config['PACKET']['LenTTL'])) and (len(self.NextHop) == int(config['PACKET']['LenNextHop'])) and (len(self.Payload) <= int(config['PACKET']['LenPayload'])) ) :
-            frame1 = bytearray(self.NetId,'utf-8')
-            frame2 = bytearray(self.Length,'utf-8')
-            frame3 = bytearray(self.Destination,'utf-8')
-            frame4 = bytearray(self.Source,'utf-8')
-            frame5 = bytearray(self.Type,'utf-8')
-            frame6 = bytearray(self.TTL,'utf-8')
-            frame7 = bytearray(self.NextHop,'utf-8')
-            
-            if isinstance (self.Payload, str):
-                frame8 = bytearray(self.Payload,'utf-8')
+
+        if ((len(self.NetId) == int(config['PACKET']['LenNetId']))
+                and (len(self.Length) == int(config['PACKET']['LenLength']))
+                and
+            (len(self.Destination) == int(config['PACKET']['LenDestination']))
+                and (len(self.Source) == int(config['PACKET']['LenSource']))
+                and (len(self.Type) == int(config['PACKET']['LenType']))
+                and (len(self.TTL) == int(config['PACKET']['LenTTL']))
+                and (len(self.NextHop) == int(config['PACKET']['LenNextHop']))
+                and
+            (len(self.Payload) <= int(config['PACKET']['LenPayload']))):
+            frame1 = bytearray(self.NetId, 'utf-8')
+            frame2 = bytearray(self.Length, 'utf-8')
+            frame3 = bytearray(self.Destination, 'utf-8')
+            frame4 = bytearray(self.Source, 'utf-8')
+            frame5 = bytearray(self.Type, 'utf-8')
+            frame6 = bytearray(self.TTL, 'utf-8')
+            frame7 = bytearray(self.NextHop, 'utf-8')
+
+            if isinstance(self.Payload, str):
+                frame8 = bytearray(self.Payload, 'utf-8')
             else:
-                frame8=bytearray()
+                frame8 = bytearray()
                 frame8.extend(self.Payload)
 
-            
-
-
-          
             frame = frame1 + frame2 + frame3 + frame4 + frame5 + frame6 + frame7 + frame8
-            print(frame)
+            if config['DEBUG']['PRINT_LOGS'] is True:
+                print(frame)
             return frame
         else:
-            print("Error Packet Size")
-                
+            if config['DEBUG']['PRINT_LOGS'] is True:
+                print("Error Packet Size")
+
     def fixTheLen(self):
         fix1 = int(config['PACKET']['LenNetId']) - len(self.NetId)
         if (fix1 != 0):
             while fix1 > 0:
                 self.NetId = "-" + self.NetId
-                fix1=fix1 - 1
+                fix1 = fix1 - 1
                 fix1 = int(config['PACKET']['LenNetId']) - len(self.NetId)
 
         fix1 = int(config['PACKET']['LenLength']) - len(self.Length)
         if (fix1 != 0):
             while fix1 > 0:
                 self.Length = "-" + self.Length
-                fix1=fix1 - 1
+                fix1 = fix1 - 1
 
         fix1 = int(config['PACKET']['LenDestination']) - len(self.Destination)
         if (fix1 != 0):
             while fix1 > 0:
                 self.Destination = "-" + self.Destination
-                fix1=fix1 - 1
+                fix1 = fix1 - 1
 
         fix1 = int(config['PACKET']['LenSource']) - len(self.Source)
         if (fix1 != 0):
             while fix1 > 0:
                 self.Source = "-" + self.Source
-                fix1=fix1 - 1
+                fix1 = fix1 - 1
 
         fix1 = int(config['PACKET']['LenType']) - len(self.Type)
         if (fix1 != 0):
             while fix1 > 0:
                 self.Type = "-" + self.Type
-                fix1=fix1 - 1
+                fix1 = fix1 - 1
 
         fix1 = int(config['PACKET']['LenTTL']) - len(self.TTL)
         if (fix1 != 0):
             while fix1 > 0:
                 self.TTL = "-" + self.TTL
-                fix1=fix1 - 1
+                fix1 = fix1 - 1
 
         fix1 = int(config['PACKET']['LenNextHop']) - len(self.NextHop)
         if (fix1 != 0):
             while fix1 > 0:
                 self.NextHop = "-" + self.NextHop
-                fix1=fix1 - 1
+                fix1 = fix1 - 1
 
         # fix1 = int(config['PACKET']['LenPayload']) - len(self.Payload)
         # if (fix1 != 0):
@@ -156,41 +169,46 @@ class Packets:
         #         fix1=fix1 - 1
 
     def LengthCalcolator(self):
-        temp = len(self.NetId) + len(self.Destination) + len(self.Source) +len(self.Type) + len(self.TTL) +len(self.NextHop) +len(self.Payload)
+        temp = len(self.NetId) + len(self.Destination) + len(
+            self.Source) + len(self.Type) + len(self.TTL) + len(
+                self.NextHop) + len(self.Payload)
         #print(temp)
         self.Length = str(temp)
-    
+
     def DecreaseTTL(self):
         temp = int(self.TTL) - 1
         self.TTL = str(temp)
-    
-    def ChangeDst(self,NewDest):
+
+    def ChangeDst(self, NewDest):
         self.Destination = NewDest
 
+
 class BeaconPacket(Packets):
-    
-    def __init__(self,NetId,Destination,Source,TTL,NextHop,Payload):
+    def __init__(self, NetId, Destination, Source, TTL, NextHop, Payload):
         #super().__init__(NetId,"",Destination,Source,"0",TTL,NextHop,"Payload BEACON")
-        super().__init__(NetId,"",Destination,Source,"0",TTL,NextHop,Payload)  #0 beacon
+        super().__init__(NetId, "", Destination, Source, "0", TTL, NextHop,
+                         Payload)  #0 beacon
         super().LengthCalcolator()
+
 
 class ReportPacket(Packets):
-    
-    def __init__(self,NetId,Destination,Source,TTL,NextHop,Payload):
-        super().__init__(NetId,"",Destination,Source,"1",TTL,NextHop,Payload)  #1 report
+    def __init__(self, NetId, Destination, Source, TTL, NextHop, Payload):
+        super().__init__(NetId, "", Destination, Source, "1", TTL, NextHop,
+                         Payload)  #1 report
         super().LengthCalcolator()
+
 
 class DataPacket(Packets):
-    
-    def __init__(self,NetId,Destination,Source,TTL,NextHop,Payload):
+    def __init__(self, NetId, Destination, Source, TTL, NextHop, Payload):
         #super().__init__(NetId,"",Destination,Source,"2",TTL,NextHop,"Payload DATA")  #2 data
-        super().__init__(NetId,"",Destination,Source,"2",TTL,NextHop,Payload)  #2 data
+        super().__init__(NetId, "", Destination, Source, "2", TTL, NextHop,
+                         Payload)  #2 data
         super().LengthCalcolator()
 
 
-class FunctionPacket(Packets):    
-    def __init__(self,NetId,Destination,Source,TTL,NextHop,Payload):
+class FunctionPacket(Packets):
+    def __init__(self, NetId, Destination, Source, TTL, NextHop, Payload):
         #super().__init__(NetId,"",Destination,Source,"2",TTL,NextHop,"Payload DATA")  #2 data
-        super().__init__(NetId,"",Destination,Source,"3",TTL,NextHop,Payload)  #2 data
+        super().__init__(NetId, "", Destination, Source, "3", TTL, NextHop,
+                         Payload)  #2 data
         super().LengthCalcolator()
-
