4c69e0dbe01707e7d83b7ded3662a8117284f19e
Marco Cavalli <cmarcos46@gmail.com>
2021-08-26 13:49:22 +0200

    Now the AudioFile has a consumer-producer logic: 2 threads, one records the chunks of audio and put in a queue, the other reads the queue and sends the chunks to the sink

--- AudioFile.py
+++ AudioFile.py
@@ -1,137 +1,99 @@
-from multiprocessing import Process
-import os
-import subprocess
-import wave
 import threading
-import socket
-import sys
-import time
-import PacketsHandler
 import UDP_Socket
-from Packets import ReportPacket
-from Packets import BeaconPacket
-from Packets import Packets
 from Packets import DataPacket
 import node_variables
 import init_config
 from configparser import ConfigParser
 import serial
+from queue import Queue
 
 config = ConfigParser()
 config.read('config.ini')
 
-# class ThreadSendDataAudio (threading.Thread):
-#   def __init__(self, threadID, name):
-#      threading.Thread.__init__(self)
-#     self.threadID = threadID
-#    self.name = name
-
-# def run(self):
-#   print("Starting " + self.name)
-#  #UdpSocketReceiver( config.get(socket.gethostname(),'IpStation') , int(config['GENERAL']['Port']) )
-# AudioFile()
-
 
 def GetAudio(action):
-    # time.sleep(5)
-    #nodeIP = init_config.GetIp(config['GENERAL']['StationInterface'] )
-    # while True:
 
     if action.decode('utf-8') == "ON":
 
-        t = ThreadAud("AudioThread", action)
+        t = ThreadAud("AudioThread", action, q)
         t.start()
-        # audioT.join()
         print("Thread Audio Started")
-        # return
 
     if action.decode('utf-8') == "OFF":
         print("Microphone " + action.decode('utf-8'))
         node_variables.MicStatus = action
-        #os.system("kill -SIGKILL " + str(node_variables.ThreadId))
-
-        #obj = wave.open('/tmp/SDNPy-master/sound.wav', 'r')
-    # print("Number of channels", obj.getnchannels())
-    #print("Sample width", obj.getsampwidth())
-    #print("Frame rate.", obj.getframerate())
-    #print("Number of frames", obj.getnframes())
-    #print("parameters:", obj.getparams())
-    #frame = 0
-    # for x in range(0, 2601600, 80):
-    #tmp = obj.readframes(int(config['FileWave']['Frame']))
-    #pckData = DataPacket(config['GENERAL']['NetId'],config['GENERAL']['IpSink'], nodeIP, "100",config['GENERAL']['IpSink'],'CIAOOONE')
-    # UDP_Socket.SendUdpPacketUnicast(pckData.getBytesFromPackets(),config['GENERAL']['IpSink'],int(config['GENERAL']['Port']))
-    #frame = frame + len(tmp)
-    #print("Frame ["+ str(x) + "]   lette: " + str(frame))
-    # time.sleep(float(config['FileWave']['TimeSleepBetweenTwoFrame']))
-    # obj.close()
-    # time.sleep(int(config['FileWave']['TimeSleepBetweenTwoPlay']))
 
 
 class ThreadAud(threading.Thread):
-    def __init__(self, name, action):
+    def __init__(self, name, action, queue):
         threading.Thread.__init__(self)
         self.name = name
         self.action = action.decode('utf-8')
+        self.queue = queue
 
     def run(self):
         node_variables.MicStatus = self.action
-        if config.getboolean('DEBUG','PRINT_LOGS') is True:
+        if config['DEBUG']['PRINT_LOGS'] is True:
             print("Avvio il Thread all'interno")
-        ser = serial.Serial('/dev/ttyUSB1',
-                            baudrate=12000000,
-                            rtscts=True,
-                            dsrdtr=True,
-                            timeout=0)
-        if config.getboolean('DEBUG','PRINT_LOGS') is True:
-            print(ser.isOpen())
-        pos = 0
-        buffSize = 3
-        inBuff = [bytes(0)] * buffSize
-        messRCV = False
-        sampleSizeRCV = False
-        audioSample = []
-        if config.getboolean('DEBUG','PRINT_LOGS') is True:
-            print("Eccomi")
+        buffSize = 360
+        port = '/dev/ttyACM0'
+        baudrate = 4000000
+        audioSample = bytearray()
+
+        ser = serial.Serial(None, baudrate, timeout = 0.1)
+        ser.port = port
+        ser.open()
+        counter_packets = 0
+        print("Microphone " + self.action)     
+
         while node_variables.MicStatus == "ON":
-            print("Microphone " + self.action)
-            #audioSample = []
-            audioSample = bytearray()
-            while len(audioSample) <= 1000:
-                messRCV = False
-                strVal = ser.read(1)
-                #print(strVal)
-                if strVal == ('<').encode():
-                    while not messRCV:
-                        if pos == buffSize:  # and val == ('\r').encode():
+            ser.write('start'.encode('utf-8'))
+            bufferedData = bytearray()
+            counter = 0
+            while counter < 5:
+                inBuff = ser.read(buffSize)
+                bufferedData.extend(inBuff)
+                counter += 1
+            self.queue.put(bufferedData)
+            audioSample.extend(bufferedData)
+            counter_packets += 1
+            #print("AUDIO: ", len(inBuff))
 
-                            #print("---------->", int.from_bytes(inBuff[0] + inBuff[1] + inBuff[2], "big", signed="True") )
-                            if config.getboolean('DEBUG','PRINT_LOGS') is True:
-                                print("---------->",
-                                      inBuff[0] + inBuff[1] + inBuff[2])
-                            audioSample.extend(inBuff[0] + inBuff[1] +
-                                               inBuff[2])
-                            #audioSample.append(int.from_bytes(inBuff[0] + inBuff[1] + inBuff[2], "big", signed="True"))
-                            messRCV = True
-                            pos = 0
-                            inBuff = [bytes(0)] * buffSize
-                        else:
-                            val = ser.read(1)
-                            inBuff[pos] = bytes(val)
-                            pos = pos + 1
+        ser.write('stop'.encode('utf-8'))
+        print("Packets sent: {}".format(counter_packets))
+# SCRITTURA SU FILE NELLA onion
+#        fHandler = open('/etc/SDNPy-SDNWiMesh/audio.bin', 'wb')
+#        fHandler.write(audioSample)
+#        fHandler.close()
 
-            if config.getboolean('DEBUG','PRINT_LOGS') is True:
-                print("send mic data:", str(audioSample))
+        ser.close()
+
+
+class ThreadWriter(threading.Thread):
+    def __init__(self, name, queue):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.queue = queue
+
+    def run(self):
+        while True:
+            print("Elements in queue: {}".format(self.queue.qsize()))
+            bufferedData = self.queue.get()
+            
             pckData = DataPacket(
                 config['GENERAL']['NetId'], config['GENERAL']['IpSink'],
                 init_config.GetIp(config['GENERAL']['StationInterface']),
-                "100", config['GENERAL']['IpSink'], audioSample)
-            # pckData="-1--38----192.168.3.1----192.168.3.1-2100----192.168.3.1"+payload
-            # pckData=pckData.replace("192.168.3.1-2100",init_config.GetIp(config['GENERAL']['StationInterface'])+"-2100")
-            #pckData=pckData.replace("38", str(38+len(payload)))
+                "100", config['GENERAL']['IpSink'], bufferedData)
 
             UDP_Socket.SendUdpPacketUnicast(pckData.getBytesFromPackets(),
                                             config['GENERAL']['IpSink'],
                                             int(config['GENERAL']['Port']))
 
-            # UDP_Socket.SendUdpPacketUnicast(pckData.getBytesFromPackets(),config['GENERAL']['IpSink'],int(config['GENERAL']['Port']))
+
+q = Queue()
+tw = ThreadWriter("WriterThread", q)
+tw.start()
+
+
+
+
