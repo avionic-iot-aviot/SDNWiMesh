d8326a988d60d20f22806d1e26e611f46e1aadf5
Marco Cavalli <cmarcos46@gmail.com>
2021-09-14 19:27:46 +0200

    Now main.py is able to get IPs from hostnames (useful to retrieve the wan IPs for gw and sink)

--- main.py
+++ main.py
@@ -21,6 +21,12 @@ neighbours=Queue()
 
 print("\n\n\t\tSTART SDNWISE\n\n")
 
+hostname=config['GENERAL']['IPRasp']
+config['GENERAL']['IPRasp']=socket.gethostbyname(hostname)
+hostname=config['GENERAL']['IpSinkOnWan']
+config['GENERAL']['IpSinkOnWan']=socket.gethostbyname(hostname)
+print("Ip Rasp on Wan: {}".format(config['GENERAL']['IPRasp']))
+print("Ip Sink on Wan: {}".format(config['GENERAL']['IPSinkOnWan']))
 #init_config.SetDeviceOnStart()
 
 #print("\n\tIndirizzi Ip Asseganti\n")
