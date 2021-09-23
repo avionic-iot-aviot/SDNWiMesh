fe1ee540415be083014028cce258d0d2da735fb3
Marco Cavalli <cmarcos46@gmail.com>
2021-09-23 10:03:45 +0200

    Only the Sink can (and have to) resolve names. Now it won't run this code for simple onions

--- PacketsHandler.py
+++ PacketsHandler.py
@@ -18,12 +18,13 @@ from Packets import DataPacket
 # verifco se il pacchetto è per me o no
 
 try:
-    hostname=config['GENERAL']['IPRasp']
-    config['GENERAL']['IPRasp']=socket.gethostbyname(hostname)
-    hostname=config['GENERAL']['IpSinkOnWan']
-    config['GENERAL']['IpSinkOnWan']=socket.gethostbyname(hostname)
-    print("Ip Rasp on Wan: {}".format(config['GENERAL']['IPRasp']))
-    print("Ip Sink on Wan: {}".format(config['GENERAL']['IPSinkOnWan']))
+    if str(init_config.GetIp(config['GENERAL']['StationInterface'])) == str(config['GENERAL']['IpSink']):
+        hostname=config['GENERAL']['IPRasp']
+        config['GENERAL']['IPRasp']=socket.gethostbyname(hostname)
+        hostname=config['GENERAL']['IpSinkOnWan']
+        config['GENERAL']['IpSinkOnWan']=socket.gethostbyname(hostname)
+        print("Ip Rasp on Wan: {}".format(config['GENERAL']['IPRasp']))
+        print("Ip Sink on Wan: {}".format(config['GENERAL']['IPSinkOnWan']))
 except:
     print("Ip resolution can't be perfomed on normal Onions!")
 
